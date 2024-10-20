ddeabec3360e8421a79a8d0528c7b4682cd91ffb
Mike Curtis <mikecurtis1@gmail.com>
2024-10-20 19:47:39 -0400

    renamed function

--- PitchSet.js
+++ PitchSet.js
@@ -93,6 +93,50 @@ const ChromaticOrder = {
     'B':12
 };
 
+const Ordinals = {
+    1:'unison',
+    2:'second',
+    3:'third',
+    4:'fourth',
+    5:'fifth',
+    6:'sixth',
+    7:'seventh',
+    8:'eighth',
+    9:'ninth',
+    10:'tenth',
+    11:'eleventh',
+    12:'twelfth',
+    13:'thirteenth',
+    14:'fourteenth',
+    15:'fifteenth'
+};
+
+const Tuples = {
+    1:'single',
+    2:'double',
+    3:'triple',
+    4:'quadruple',
+    5:'quintuple',
+    6:'sextuple',
+    7:'septuple',
+    8:'octuple',
+    9:'nonuple',
+    10:'decuple'
+};
+
+const Accidentals = {
+    'double flat':'𝄫',
+    'flat':'♭',
+    'natural':'♮',
+    'sharp':'♯',
+    'double sharp':'𝄪',
+    '𝄫':'double flat',
+    '♭':'flat',
+    '♮':'natural',
+    '♯':'sharp',
+    '𝄪':'double sharp'
+};
+
 class Pitch {
     constructor(str) {
         this.string = str;
@@ -100,6 +144,9 @@ class Pitch {
         this.accidentals = str.substring(1,str.length-1);
         this.octave = limitOctave(Number(str.substring(str.length-1)));
         this.halfStepAlterations = quantifyAccidentals(this.accidentals);
+		this.chromaticPos = calcPitchPosChromatic(this.letter, this.octave, this.halfStepAlterations);
+		this.pianoKey = expressPianoKey(this.chromaticPos);
+		this.midiNumber = expressMidiNumber(this.chromaticPos);
     }
 };
 
@@ -109,9 +156,139 @@ class Interval {
         this.quality = str.match(/^[mMPdA]+/)[0];
         this.number = str.match(/(\d+)$/)[0];
         this.simple = calcSimpleClass(this.number);
-        this.normalized = normalizeCompoundClass(this.number);
-        this.halfSteps = calcHalfStepsFromCodes(this.simple, this.normalized, this.quality);
+        this.simpleNormalized = normalizeCompoundClass(this.number);
+        this.halfSteps = calcHalfStepsFromCodes(this.simple, this.simpleNormalized, this.quality);
+		this.halfStepsNormalized = calcHalfStepsFromCodes(this.simple, this.simpleNormalized, this.quality, true);
     }
+};
+
+const expressPianoKey = function(chromaticPos=0) {
+    let pianoKey = Number(chromaticPos) - 9;
+    return pianoKey;
+};
+
+const expressMidiNumber = function(chromaticPos=0) {
+    let midiNumber = Number(chromaticPos) + 11;
+    return midiNumber;
+};
+
+const expressOrdinal = function (n, long=false) {
+    n = Number(n);
+    let str = '';
+    if ( n >=1 && n <= 15 && long === true) {
+        return Ordinals[n];
+    }
+    if ( n > 0 ) {
+        if ( n >= 4 && n <= 20) {
+            str =  n.toString() + 'th';
+        } else {
+            let f = n % 10;
+            if ( f === 1 ) {
+                str =  n.toString() + 'st';
+            } else if ( f === 2 ) {
+                str =  n.toString() + 'nd';
+            } else if ( f === 3 ) {
+                str =  n.toString() + 'rd';
+            } else {
+                str =  n.toString() + 'th';
+            }
+        }
+    } else {
+        str = n.toString();
+    }
+    return str;
+};
+
+const expressTuple = function (str) {
+    let prepended = '';
+    str = str.toString();
+    // Javascript Unicode encoding is UTF-16
+    // UTF-16 double-flat & double-sharp are each one code point of two code units
+    // str.length counts code units
+    // Array.from breaks a string on code points
+    let len = Array.from(str).length;
+    let a = Array.from(str)[0];
+    let regex = new RegExp('^' + a + '{2,}$', 'u');
+    if ( str.match(regex) !== null && Tuples[len] ) {
+        prepended = Tuples[len] + ' ' + a; 
+    } else {
+        prepended = str;
+    }
+    return prepended;
+};
+
+const limitOctave = function (n){
+    let octaveNum = Number(n);
+    if (octaveNum < 0) {
+        return 0;
+    } else if (octaveNum > 8) {
+        return 8;
+    } 
+    return octaveNum;
+};
+
+const normalizeCompoundClass = function (n) {
+    n = Number(n);
+    let normalized = n;
+    if (n > 15) {
+        let f = Math.floor(n / 7);
+        if (n % 7 === 1 || n % 7 === 0) {
+            normalized = normalized - (f - 2) * 7;
+        } else {
+            normalized = normalized - (f - 1) * 7;
+        }
+    }
+    return normalized;
+};
+
+const normalizeCompoundHalfSteps = function(n){
+    n = Number(n);
+    let normalized = n;
+    if (n > 24) {
+        let f = Math.floor(n / 12);
+        if (n % 12 === 1 || n % 12 === 0) {
+            normalized = normalized - (f - 2) * 12;
+        } else {
+            normalized = normalized - (f - 1) * 12;
+        }
+    }
+    return normalized;
+};
+
+const normalizeDoubleAccidental = function (str) {
+    let accidental = '';
+    if ( str === '♭♭' ) {
+        accidental = '𝄫';
+    } else if ( str === '♯♯' ) {
+        accidental = '𝄪';
+    } else {
+        return str;
+    }
+    return accidental;
+};
+
+const quantifyAccidentals = function (str){
+    let halfSteps = 0;
+    Array.from(str).forEach(
+        function(chr){
+            if(chr==='𝄫'){
+                halfSteps = halfSteps - 2;
+            }
+            if(chr==='♭'){
+                halfSteps = halfSteps - 1;
+            }
+            if(chr==='♮'){
+                halfSteps = halfSteps;
+            }
+            if(chr==='♯'){
+                halfSteps = halfSteps + 1;
+            }
+            if(chr==='𝄪'){
+                halfSteps = halfSteps + 2;
+            }
+        }
+    );
+    return halfSteps;
 };
 
 const calcPitchPos = function(letter, octave) {
@@ -150,22 +327,12 @@ const calcPosOrder = function (pos) {
     return order
 };
 
-const calcPitchPosChromatic = function(letter, octave) {
+const calcPitchPosChromatic = function(letter, octave, halfsteps=0) {
     let pitchPos = 0;
     let order = 0;
     order = ChromaticOrder[letter];
-    pitchPos = order + (octave * 12);
+    pitchPos = order + (octave * 12) + halfsteps;
     return pitchPos;
-};
-
-const limitOctave = function (n){
-    let octaveNum = Number(n);
-    if (octaveNum < 0) {
-        return 0;
-    } else if (octaveNum > 8) {
-        return 8;
-    } 
-    return octaveNum;
 };
 
 const calcSimpleClass = function (n) {
@@ -181,34 +348,6 @@ const calcSimpleClass = function (n) {
     return simple;
 };
 
-const normalizeCompoundClass = function (n) {
-    n = Number(n);
-    let normalized = n;
-    if (n > 15) {
-        let f = Math.floor(n / 7);
-        if (n % 7 === 1 || n % 7 === 0) {
-            normalized = normalized - (f - 2) * 7;
-        } else {
-            normalized = normalized - (f - 1) * 7;
-        }
-    }
-    return normalized;
-};
-
-const normalizeCompoundHalfSteps = function(n){
-    n = Number(n);
-    let normalized = n;
-    if (n > 24) {
-        let f = Math.floor(n / 12);
-        if (n % 12 === 1 || n % 12 === 0) {
-            normalized = normalized - (f - 2) * 12;
-        } else {
-            normalized = normalized - (f - 1) * 12;
-        }
-    }
-    return normalized;
-};
-
 const calcIntervalClass = function (p1, p2){
     let intervalClass = 0;
     let pos1 = calcPitchPos(p1.letter, p1.octave);
@@ -218,53 +357,19 @@ const calcIntervalClass = function (p1, p2){
     return intervalClass;
 };
 
-const quantifyAccidentals = function (str){
+const calcHalfStepsFromPitches = function (p1, p2, normalize=false){
     let halfSteps = 0;
-    Array.from(str).forEach(
-        function(chr){
-            if(chr==='𝄫'){
-                halfSteps = halfSteps - 2;
-            }
-            if(chr==='♭'){
-                halfSteps = halfSteps - 1;
-            }
-            if(chr==='♮'){
-                halfSteps = halfSteps;
-            }
-            if(chr==='♯'){
-                halfSteps = halfSteps + 1;
-            }
-            if(chr==='𝄪'){
-                halfSteps = halfSteps + 2;
-            }
-        }
-    );
-    return halfSteps;
-};
-
-const calcHalfStepsFromPitches = function (p1, p2, accidentalsTotal=false){
-    let halfSteps = 0;
-    let pos1 = calcPitchPosChromatic(p1.letter, p1.octave);
-    let pos2 = calcPitchPosChromatic(p2.letter, p2.octave);
-    let diff = Math.abs(pos1 - pos2);
-    halfSteps = diff;
-    if (pos1 < pos2) {
-        halfSteps = halfSteps - p1.halfStepAlterations;
-        halfSteps = halfSteps + p2.halfStepAlterations;
-    } 
-    if (pos1 > pos2) {
-        halfSteps = halfSteps + p1.halfStepAlterations;
-        halfSteps = halfSteps - p2.halfStepAlterations;
-    }
-    if (accidentalsTotal === true) {
-        return halfSteps - diff;
+    let pos1 = calcPitchPosChromatic(p1.letter, p1.octave, p1.halfStepAlterations);
+    let pos2 = calcPitchPosChromatic(p2.letter, p2.octave, p2.halfStepAlterations);
+	halfSteps = Math.abs(pos1 - pos2);
+    if (normalize === true) {
+		return normalizeCompoundHalfSteps(halfSteps);
     } else {
-        halfSteps = normalizeCompoundHalfSteps(halfSteps);
-        return halfSteps;
+		return halfSteps;
     }
 };
 
-const calcHalfStepsFromCodes = function (simple, normalized, quality) {
+const calcHalfStepsFromCodes = function (simple, normalized, quality, normalize=false) {
     let halfSteps = 0;
     if ( FiniteIntervals[simple] ) {
         if ( quality.match(/^d+$/g) ) {
@@ -279,18 +384,20 @@ const calcHalfStepsFromCodes = function (simple, normalized, quality) {
             halfSteps = FiniteIntervals[simple][quality];
         }
     }
-    let q = normalized / 8;
-    if ( q >= 1 ) {
-        halfSteps = halfSteps + (Math.ceil(q) - 1) * 12
-    }
+	if ( normalize === true ) {
+		let q = normalized / 8;
+		if ( q >= 1 ) {
+			halfSteps = halfSteps + (Math.ceil(q) - 1) * 12
+		}
+	}
     return halfSteps;
 };
 
 const buildIntervalName = function(pitch1, pitch2){
     let intervalName = '';
-    let accidentalsTotal = calcHalfStepsFromPitches(pitch2, pitch1, true);
+    let accidentalsTotal = calcHalfStepsFromPitches(pitch2, pitch1);
     let intervalClass = calcIntervalClass(pitch1, pitch2);
-    let halfSteps = calcHalfStepsFromPitches(pitch1, pitch2);
+    let halfSteps = calcHalfStepsFromPitches(pitch1, pitch2, true);
     let simpleClass = calcSimpleClass(Number(intervalClass));
     let simpleHalfSteps = 0;
     if (intervalClass > 8) {
@@ -315,25 +422,22 @@ const buildIntervalName = function(pitch1, pitch2){
     return intervalName;
 };
 
-const harmonizePitch = function (pitch1, interval) {
+const transposePitch = function (pitch1, interval) {
     let pitch2 = null;
-    let pitch2String = '';
-    let accidentals = '';
-    let pos1 = calcPitchPos(pitch1.letter, pitch1.octave);
-    let pos2 = pos1 + Number(interval.number) - 1;
+	let pos1 = calcPitchPos(pitch1.letter, pitch1.octave);
+	let pos2 = pos1 + Number(interval.simpleNormalized) - 1;
     let letter2 = calcPosLetter(pos2);
-    let octave = calcPosOctave(pos2);
-    let tempPitch2 = new Pitch(letter2 + '' + octave);
-    let tempSize = calcHalfStepsFromPitches(pitch1, tempPitch2);
-    let diff = interval.halfSteps - tempSize;
-    if (diff < 0) {
+    let octave2 = calcPosOctave(pos2);
+	let tempHalfSteps = calcHalfStepsFromPitches(pitch1, new Pitch(letter2 + '' + octave2), true);
+	let diff = interval.halfStepsNormalized - tempHalfSteps;
+	let accidentals = '';
+	if (diff < 0) {
         accidentals = String('♭').repeat(Math.abs(diff));
     } else if (diff > 0){
         accidentals = String('♯').repeat(Math.abs(diff));
     } else {
         accidentals = '♮';
     }
-    pitch2String = letter2 + accidentals + octave;
-    pitch2 = new Pitch(pitch2String);
-    return pitch2;
+	pitch2 = new Pitch(letter2 + accidentals + octave2);
+	return pitch2;
 };
