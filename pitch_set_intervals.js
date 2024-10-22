ca01b5d557a8339eca12276fe72212255799dd55
Mike Curtis <mikecurtis1@gmail.com>
2024-10-22 16:29:25 -0400

    whitespace edit

--- pitch_set_intervals.js
+++ pitch_set_intervals.js
@@ -1,39 +1,39 @@
 const PitchSetIntervals = {
 	"major_scale": {
-        "tonic": "P1",
-        "second": "M2",
-        "third": "M3",
-        "fourth": "P4",
+		"tonic": "P1",
+		"second": "M2",
+		"third": "M3",
+		"fourth": "P4",
 		"fifth": "P5",
 		"sixth": "M6",
 		"seventh": "M7",
 		"octave": "P8"
-    },
+	},
 	"natural_minor_scale": {
-        "tonic": "P1",
-        "second": "M2",
-        "third": "m3",
-        "fourth": "P4",
+		"tonic": "P1",
+		"second": "M2",
+		"third": "m3",
+		"fourth": "P4",
 		"fifth": "P5",
 		"sixth": "m6",
 		"seventh": "m7",
 		"octave": "P8"
-    },
+	},
 	"harmonic_minor_scale": {
-        "tonic": "P1",
-        "second": "M2",
-        "third": "m3",
-        "fourth": "P4",
+		"tonic": "P1",
+		"second": "M2",
+		"third": "m3",
+		"fourth": "P4",
 		"fifth": "P5",
 		"sixth": "m6",
 		"seventh": "M7",
 		"octave": "P8"
-    },
+	},
 	"melodic_minor_scale": {
-        "tonic_ascending": "P1",
-        "second_ascending": "M2",
-        "third_ascending": "m3",
-        "fourth_ascending": "P4",
+		"tonic_ascending": "P1",
+		"second_ascending": "M2",
+		"third_ascending": "m3",
+		"fourth_ascending": "P4",
 		"fifth_ascending": "P5",
 		"sixth_ascending": "M6",
 		"seventh_ascending": "M7",
@@ -42,16 +42,16 @@ const PitchSetIntervals = {
 		"seventh_descending": "m7",
 		"sixth_descending": "m6",
 		"fifth_descending": "P5",
-        "fourth_descending": "P4",
-        "third_descending": "m3",
-        "second_descending": "M2",
-        "tonic_descending": "P1"
-    },
+		"fourth_descending": "P4",
+		"third_descending": "m3",
+		"second_descending": "M2",
+		"tonic_descending": "P1"
+	},
 	"lydian_dominant_scale": {
 		"tonic": "P1",
-        "second": "M2",
-        "third": "M3",
-        "fourth": "A4",
+		"second": "M2",
+		"third": "M3",
+		"fourth": "A4",
 		"fifth": "P5",
 		"sixth": "M6",
 		"seventh": "m7",
@@ -59,19 +59,19 @@ const PitchSetIntervals = {
 	},
 	"Freygish_scale": {
 		"tonic": "P1",
-        "second": "m2",
-        "third": "M3",
-        "fourth": "P4",
+		"second": "m2",
+		"third": "M3",
+		"fourth": "P4",
 		"fifth": "P5",
 		"sixth": "m6",
 		"seventh": "m7",
 		"octave": "P8"
 	},
 	"double_harmonic_scale": {
-        "tonic": "P1",
-        "second": "m2",
-        "third": "M3",
-        "fourth": "P4",
+		"tonic": "P1",
+		"second": "m2",
+		"third": "M3",
+		"fourth": "P4",
 		"fifth":"P5",
 		"sixth":"m6",
 		"seventh": "M7",
@@ -165,29 +165,29 @@ const PitchSetIntervals = {
 		"P8": "P8"
 	},
 	"major_tetrachord": {
-        "tonic": "P1",
-        "second": "M2",
-        "third": "M3",
-        "fourth": "P4"
-    },
+		"tonic": "P1",
+		"second": "M2",
+		"third": "M3",
+		"fourth": "P4"
+	},
 	"minor_tetrachord": {
-        "tonic": "P1",
-        "second": "M2",
-        "third": "m3",
-        "fourth": "P4"
-    },
+		"tonic": "P1",
+		"second": "M2",
+		"third": "m3",
+		"fourth": "P4"
+	},
 	"phrygian_tetrachord": {
-        "tonic": "P1",
-        "second": "m2",
-        "third": "m3",
-        "fourth": "P4"
-    },
+		"tonic": "P1",
+		"second": "m2",
+		"third": "m3",
+		"fourth": "P4"
+	},
 	"harmonic_tetrachord": {
-        "tonic": "P1",
-        "second": "m2",
-        "third": "M3",
-        "fourth": "P4"
-    },
+		"tonic": "P1",
+		"second": "m2",
+		"third": "M3",
+		"fourth": "P4"
+	},
 	"Common Intervals": {
 		"P1": "P1",
 		"m2": "m2",
@@ -208,145 +208,145 @@ const PitchSetIntervals = {
 		"M7": "M7",
 		"P8": "P8"
 	},
-    "major_triad": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5"
-    },
-    "minor_triad": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "P5"
-    },
-    "augmented_triad": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "A5"
-    },
-    "diminished_triad": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "d5"
-    },
-    "dominant_seventh_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "m7"
-    },
+	"major_triad": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5"
+	},
+	"minor_triad": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "P5"
+	},
+	"augmented_triad": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "A5"
+	},
+	"diminished_triad": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "d5"
+	},
+	"dominant_seventh_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "m7"
+	},
 	"dominant_seventh_flat_five_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "d5",
-        "seventh": "m7"
-    },
-    "augmented_seventh_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "A5",
-        "seventh": "m7"
-    },
-	"minor_seventh_chord": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "P5",
-        "seventh": "m7"
-    },
-	"minor_major_seventh_chord": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "P5",
-        "seventh": "M7"
-    },
-    "major_seventh_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "M7"
-    },
-    "diminished_seventh_chord": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "d5",
-        "seventh": "d7"
-    },
-    "half_diminished_seventh_chord": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "d5",
-        "seventh": "m7"
-    },
+		"root": "P1",
+		"third": "M3",
+		"fifth": "d5",
+		"seventh": "m7"
+	},
 	"augmented_seventh_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "A5",
-        "seventh": "m7"
-    },
+		"root": "P1",
+		"third": "M3",
+		"fifth": "A5",
+		"seventh": "m7"
+	},
+	"minor_seventh_chord": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "P5",
+		"seventh": "m7"
+	},
+	"minor_major_seventh_chord": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "P5",
+		"seventh": "M7"
+	},
+	"major_seventh_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "M7"
+	},
+	"diminished_seventh_chord": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "d5",
+		"seventh": "d7"
+	},
+	"half_diminished_seventh_chord": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "d5",
+		"seventh": "m7"
+	},
+	"augmented_seventh_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "A5",
+		"seventh": "m7"
+	},
 	"augmented_major_seventh_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "A5",
-        "seventh": "M7"
-    },
+		"root": "P1",
+		"third": "M3",
+		"fifth": "A5",
+		"seventh": "M7"
+	},
 	"Italian_augmented_sixth_chord": {
 		"root": "P1",
-        "third": "M3",
-        "sixth": "A6"
+		"third": "M3",
+		"sixth": "A6"
 	},
 	"French_augmented_sixth_chord": {
 		"root": "P1",
-        "third": "M3",
-        "fourth": "A4",
-        "sixth": "A6"
+		"third": "M3",
+		"fourth": "A4",
+		"sixth": "A6"
 	},
 	"German_augmented_sixth_chord": {
 		"root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "sixth": "A6"
+		"third": "M3",
+		"fifth": "P5",
+		"sixth": "A6"
 	},
 	"Neapolitan_sixth_chord": {
 		"root": "P1",
-        "third": "M3",
-        "fifth": "P5"
+		"third": "M3",
+		"fifth": "P5"
 	},
-    "dominant_ninth_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "m7",
-        "ninth": "M9"
-    },
-    "dominant_minor_ninth_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "m7",
-        "ninth": "m9"
-    },
-    "minor_ninth_chord": {
-        "root": "P1",
-        "third": "m3",
-        "fifth": "P5",
-        "seventh": "m7",
-        "ninth": "M9"
-    },
-    "major_ninth_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "M7",
-        "ninth": "M9"
-    },
+	"dominant_ninth_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "m7",
+		"ninth": "M9"
+	},
+	"dominant_minor_ninth_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "m7",
+		"ninth": "m9"
+	},
+	"minor_ninth_chord": {
+		"root": "P1",
+		"third": "m3",
+		"fifth": "P5",
+		"seventh": "m7",
+		"ninth": "M9"
+	},
+	"major_ninth_chord": {
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "M7",
+		"ninth": "M9"
+	},
 	"dominant_thirteenth_chord": {
-        "root": "P1",
-        "third": "M3",
-        "fifth": "P5",
-        "seventh": "m7",
-        "ninth": "M9",
+		"root": "P1",
+		"third": "M3",
+		"fifth": "P5",
+		"seventh": "m7",
+		"ninth": "M9",
 		"eleventh": "P11",
 		"thirteenth": "M13"
-    },
+	},
 	"Prometheus_chord": {
 		"root": "P1",
 		"fourth": "A4",
@@ -356,22 +356,22 @@ const PitchSetIntervals = {
 		"sixteenth": "M16"
 	},
 	"Petrushka_chord": {
-        "root1": "P1",
-        "third1": "M3",
-        "fifth1": "P5",
+		"root1": "P1",
+		"third1": "M3",
+		"fifth1": "P5",
 		"root2": "d5",
-        "third3": "m7",
-        "fifth2": "m9"
+		"third3": "m7",
+		"fifth2": "m9"
 	},
 	"quartal_triad": {
 		"root": "P1",
-        "fourth": "P4",
-        "seventh": "m7"
+		"fourth": "P4",
+		"seventh": "m7"
 	},
 	"quartal_tetrad": {
 		"root": "P1",
-        "fourth": "P4",
-        "seventh": "m7",
+		"fourth": "P4",
+		"seventh": "m7",
 		"tenth": "m10"
 	}
 };
